3eaec044c25d79c7e7f68ef43fa93cfd16c54756
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-05-28 15:28:07 -0700

    v0.1.0-alpha.33

--- bindings/datasette/datasette_sqlite_hello/version.py
+++ bindings/datasette/datasette_sqlite_hello/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.1.0-alpha.32"
+__version__ = "0.1.0-alpha.33"
 __version_info__ = tuple(__version__.split("."))
--- bindings/ruby/lib/version.rb
+++ bindings/ruby/lib/version.rb
@@ -1,4 +1,4 @@
 # automatically generated, do not edit by hand.
 module SqliteHello
-  VERSION = "0.1.0-alpha.32"
+  VERSION = "0.1.0-alpha.33"
 end
