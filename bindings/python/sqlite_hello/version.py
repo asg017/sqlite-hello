985ddf0ac91d24b5ed7f7c2875c3eddac0ece700
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-07-23 17:27:45 -0700

    v0.1.0-alpha.56

--- bindings/python/sqlite_hello/version.py
+++ bindings/python/sqlite_hello/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.1.0-alpha.55"
+__version__ = "0.1.0-alpha.56"
 __version_info__ = tuple(__version__.split("."))
--- bindings/ruby/lib/version.rb
+++ bindings/ruby/lib/version.rb
@@ -1,4 +1,4 @@
 # automatically generated, do not edit by hand.
 module SqliteHello
-  VERSION = "0.1.0-alpha.55"
+  VERSION = "0.1.0-alpha.56"
 end
